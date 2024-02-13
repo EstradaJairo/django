717be2ded272a6124697983dc7d3d212daedbaeb
EstradaJairo <destrada@jairosolutions.com>
2024-02-13 16:43:10 +0800

    Attempt Second fix build error

--- project/settings.py
+++ project/settings.py
@@ -75,13 +75,24 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'djangodb',
+#         'USER': 'postgres',
+#         'PASSWORD': '123',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'djangodb',
-        'USER': 'postgres',
-        'PASSWORD': '123',
-        'HOST': '127.0.0.1',
+        'NAME': 'verceldb',
+        'USER': 'default',
+        'PASSWORD': 'olz3yqkS7aTw',
+        'HOST': 'ep-tight-violet-a1425dd4.ap-southeast-1.aws.neon.tech',
         'PORT': '5432',
     }
 }
